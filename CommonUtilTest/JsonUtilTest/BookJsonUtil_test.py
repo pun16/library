8d5d6e9b762095bb9ab9718b55965e39b830d5fa
Tha Taerakul <pun23323@gmail.com>
2022-04-30 11:24:58 +0700

    location fix

--- CommonUtilTest/JsonUtilTest/BookJsonUtil_test.py
+++ CommonUtilTest/JsonUtilTest/BookJsonUtil_test.py
@@ -4,5 +4,5 @@ from Model.Book import Book
 
 def test_bookToJson_success():
   books = [Book("book_test"),Book("book_test2")]
-  f = open("/home/runner/14-output-ThaTaerakul/CommonUtilTest/FilesForTest/JsonUtilTest/Books.json", "r")
+  f = open("/mnt/d/data/coding_learning/library/CommonUtilTest/FilesForTest/JsonUtilTest/Books.json", "r")
   assert booksToJson(books) == f.read()
--- CommonUtilTest/JsonUtilTest/UserJsonUtil_test.py
+++ CommonUtilTest/JsonUtilTest/UserJsonUtil_test.py
@@ -4,6 +4,6 @@ from Model.User import User
 
 def test_userToJson_success():
   users = [User("User_test",1,["book"]),User("User_test2"),User("User_test3",0,["book","banana"])]
-  f = open("/home/runner/14-output-ThaTaerakul/CommonUtilTest/FilesForTest/JsonUtilTest/Users.json", "r")
+  f = open("/mnt/d/data/coding_learning/library/CommonUtilTest/FilesForTest/JsonUtilTest/Users.json", "r")
   print(usersToJson(users))
   assert usersToJson(users) == f.read()
