2b791ca3e326fd1b3b6aa19f5bfabb8b5962a6bf
Tha Taerakul <pun23323@gmail.com>
2022-06-11 11:38:07 +0700

    add config file

--- app/main/config.py
+++ app/main/config.py
@@ -3,35 +3,35 @@ import os
 # uncomment the line below for postgres database url from environment variable
 # postgres_local_base = os.environ['DATABASE_URL']
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir: str = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
-    DEBUG = False
+    SECRET_KEY: str = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG: bool = False
     # Swagger
-    RESTX_MASK_SWAGGER = False
+    RESTX_MASK_SWAGGER: bool = False
 
 
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
 
 class TestingConfig(Config):
-    DEBUG = True
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG: bool = True
+    TESTING: bool = True
+    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
+    PRESERVE_CONTEXT_ON_EXCEPTION: bool = False
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
 
 class ProductionConfig(Config):
-    DEBUG = False
+    DEBUG: bool = False
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
 
@@ -42,4 +42,4 @@ config_by_name = dict(
     prod=ProductionConfig
 )
 
-key = Config.SECRET_KEY
+key: str = Config.SECRET_KEY
